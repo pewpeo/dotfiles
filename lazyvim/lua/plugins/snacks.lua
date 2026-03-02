54501868d0f0cd3774926296f1baddd491131ddb
pewpeo <2847698+pewpeo@users.noreply.github.com>
2026-03-02 20:43:52 +0100

    Disable ignored files in snacks picker

--- lazyvim/lua/plugins/snacks.lua
+++ lazyvim/lua/plugins/snacks.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       picker = {
         hidden = true,
-        ignored = true,
+        ignored = false,
         sources = {
           files = {
             hidden = true, -- Show hidden/dotfiles
